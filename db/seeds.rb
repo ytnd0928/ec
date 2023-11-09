31dd4112c7cb7d18757c0cbb15a7188a8c822388
ytnd0928 <yetianxiongfei@gmail.com>
2023-11-10 03:42:43 +0900

    [add]add routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create([
+  { name: 'Black T-shirt', description: 'コットン素材を使用したクルーネックのカットソー。', price: '4500', image: '1.jpg' },
+  { name: 'White T-shirt', description: 'コットン素材を使用したクルーネックのカットソー。', price: '4500', image: '2.jpg' },
+  { name: 'White T-shirt 2', description: 'コットン素材を使用したクルーネックのカットソー。', price: '6800', image: '3.jpg' },
+  { name: 'Black T-shirt Plain', description: 'コットン素材を使用したクルーネックのカットソー。', price: '4800', image: '4.jpg' },
+  { name: 'Black T-shirt 2', description: 'コットン素材を使用したクルーネックのカットソー。', price: '4500', image: '5.jpg' },
+  { name: 'Navy T-shirt', description: 'コットン素材を使用したクルーネックのカットソー。', price: '4500', image: '6.jpg' },
+  { name: 'Border T-shirt', description: 'コットン素材を使用したクルーネックのカットソー。', price: '6800', image: '7.jpg' },
+  { name: 'Border Long Sleeve T-shirt', description: 'コットン素材を使用したクルーネックのカットソー。', price: '7800', image: '8.jpg' },
+  { name: 'Gray T-shirt', description: 'コットン素材を使用したクルーネックのカットソー。', price: '4500', image: '9.jpg' }
+])
