29989c83ffcc73a3dbfeed1aa140c5ea22bcfec7
ytnd0928 <yetianxiongfei@gmail.com>
2023-11-10 03:34:38 +0900

    [add]add model products

---
+++ db/migrate/20231109182618_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name, null:false
+      t.text :description, null :false
+      t.string :image, null: false
+      t.integer :price, null: false
+      
+      t.timestamps
+    end
+  end
+end
